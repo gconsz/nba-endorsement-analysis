3ce28fa9613037cb01027ba54cf67cfd23349fe4
gconsz <gconsz@gmail.com>
2026-03-09 19:54:57 -0500

    Update 01_data_exploration.sql

--- sql/sql/01_data_exploration.sql
+++ sql/sql/01_data_exploration.sql
@@ -1,1 +1,35 @@
+-- Preview for each table, check the total rows
+
+SELECT COUNT(*) AS total_rows
+FROM `smart-rope-473422-t2.NBA_ANALYSIS.games`;
+
+SELECT COUNT(*) AS total_rows
+FROM `smart-rope-473422-t2.NBA_ANALYSIS.statistics`;
+
+SELECT COUNT(*) AS total_rows
+FROM `smart-rope-473422-t2.NBA_ANALYSIS.advanced_statistics`;
+
+SELECT COUNT(*) AS total_rows
+FROM `smart-rope-473422-t2.NBA_ANALYSIS.four_factors`;
+-- Preview statistics table
+SELECT *
+FROM `smart-rope-473422-t2.NBA_ANALYSIS.statistics`
+LIMIT 10;
+
+-- Preview columns advanced statistics
+SELECT *
+FROM `smart-rope-473422-t2.NBA_ANALYSIS.advanced_statistics`
+LIMIT 10;
+
+-- Preview columns for four factors
+SELECT *
+FROM `smart-rope-473422-t2.NBA_ANALYSIS.four_factors`
+LIMIT 10;
+
+-- Preview colums for games
+SELECT *
+FROM `smart-rope-473422-t2.NBA_ANALYSIS.games`
+LIMIT 10;
+
+
 
